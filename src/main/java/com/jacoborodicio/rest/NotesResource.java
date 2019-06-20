bbc57bfc9e462077bd88b469b8c86afad6096cfd
Jacobo <jacoborodicio@gmail.com>
2019-06-20 18:35:52 +0200

    feat: Retrieving all notes under /notes/all url

--- src/main/java/com/jacoborodicio/rest/NotesResource.java
+++ src/main/java/com/jacoborodicio/rest/NotesResource.java
@@ -4,24 +4,39 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.util.List;
 
 import model.Note;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 @Path("/notes")
 public class NotesResource {
 	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("api-rest");
+	EntityManager em = emf.createEntityManager();
+	
+	
 	@GET
 	@Path("/all")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getNotes() {
-		return "This will return all notes!";
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getNotes() {
+		TypedQuery<Note> notes = this.em.createNamedQuery("Note.findAll", Note.class);
+		List<Note> totalNotes = notes.getResultList();
+		return Response.ok(totalNotes).build();
 	}
 	
 	@GET
 	@Path("/demojson")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Note demoJson() {
-		Note note = new Note("Nota inicializada a la fuerza sin interacción de la bdd", 3, "Primera nota demo");
+		Note note = new Note();
+		note.setDescription("Description of a note this time already using entities");
+		note.setTitle("Aún falta mucho pero vamos yendo!");
 		return note;
 	}
 }
--- src/main/java/model/Note.java
+++ src/main/java/model/Note.java
@@ -1,50 +1,74 @@
 package model;
 
+import java.io.Serializable;
+import javax.persistence.*;
 import java.util.Date;
 
-public class Note {
-	private String description;
-	private Integer priority;
-	private Date start_date;
-	private String title;
+
+/**
+ * The persistent class for the notes database table.
+ * 
+ */
+@Entity
+@Table(name="notes")
+@NamedQuery(name="Note.findAll", query="SELECT n FROM Note n")
+public class Note implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
 	private Integer id;
-	
-	public Note(String description, int priority, String title) {
-		this.description = description;
-		this.priority = priority;
-		this.title = title;
+
+	private String description;
+
+	private Integer priority;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="start_date")
+	private Date startDate;
+
+	private String title;
+
+	public Note() {
 	}
-	
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Integer getPriority() {
-		return priority;
-	}
-	public void setPriority(Integer priority) {
-		this.priority = priority;
-	}
-	public Date getStart_date() {
-		return start_date;
-	}
-	public void setStart_date(Date start_date) {
-		this.start_date = start_date;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
+
 	public Integer getId() {
-		return id;
+		return this.id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
-	
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPriority() {
+		return this.priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Date getStartDate() {
+		return this.startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 }
