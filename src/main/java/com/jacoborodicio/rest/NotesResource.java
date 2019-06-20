03a327a3e24b61d2b73aae078c69059a8a03095b
Jacobo <jacoborodicio@gmail.com>
2019-06-20 19:59:42 +0200

    feat: Saving single note in the database

--- src/main/java/com/jacoborodicio/rest/NotesResource.java
+++ src/main/java/com/jacoborodicio/rest/NotesResource.java
@@ -1,10 +1,11 @@
 package com.jacoborodicio.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -13,8 +14,6 @@ import java.util.List;
 import model.Note;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
@@ -47,11 +46,20 @@ public class NotesResource {
 		} catch (NoResultException ex) {
 			return Response.ok("No se han encontrado resultados con el id: "+id).build();
 		}
-//		if(finalNote != null) {
-//			System.out.println("Si hay una nota con el id especificado --> "+id);
-//			return Response.ok(finalNote).build();			
-//		} else {
-//			return Response.ok("Este es el id que entró: "+id+" pero no se encontró nada...").build();
-//		}
+	}
+	
+	@POST
+	@Path("/addNote")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addNote(Note note) {
+		EntityManager em = this.em;
+		em.getTransaction().begin();
+		em.persist(note);
+		em.flush();
+		em.getTransaction().commit();
+		em.close();
+		
+		System.out.println("Note..."+note);
+		return Response.ok("Guardando en la bdd el objeto: "+ note).build();
 	}
 }
